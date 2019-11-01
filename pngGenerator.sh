61832a638d6ad5b076d71cb3ad9f3ffb0ed72e9e
Nicolai Bakkeli <nibakkeli@gmail.com>
2019-11-01 17:31:36 +0100

    Changes to random bits and resulting file size in MB as argument

--- pngGenerator.sh
+++ pngGenerator.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
+outputDir="png/"
 
-#Bin="Binaryfil for testing innad team arkiv og digitale forsendelser." 64x3 bytes stor
+mx=$((100*"$1"))
+my=3333
 
-mx=1000
-my=1000
-bytesRequired="$((3*"$mx"*"$my"))"
-ratioString="${mx}x${my}"
-binCopies="$(( "$bytesRequired" / 15552 + 1 ))"
-
-echo "binCopies: $binCopies"
-
-printf "Creating PNG with size: %s from %s bytes.\n" ${ratioString} ${bytesRequired}
-
-for _ in $(seq 1 1 "$binCopies"); do head -c $bytesRequired pngSource.bin; done > tmp.bin
-head -c $bytesRequired tmp.bin | convert -depth 8 -size $ratioString RGB:- random.png
-rm tmp.bin
+head -c "$((3*"$mx"*"$my"))" /dev/urandom | convert -depth 8 -size "${mx}x${my}" RGB:- $outputDir/"$1"MB.png
