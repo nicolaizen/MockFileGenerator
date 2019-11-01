3ff7186f858d108d96735715901270256333e3e3
Nicolai Bakkeli <nibakkeli@gmail.com>
2019-11-01 20:39:50 +0100

    Adds root request

--- pdfGenerator.sh
+++ pdfGenerator.sh
@@ -7,7 +7,13 @@ createNoisePdf () {
   head -c "$((3*"$mx"*"$my"))" /dev/urandom | convert -depth 8 -size "${mx}x${my}" RGB:- "pdf/$1"MB.pdf
 }
 
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
 mkdir -p pdf
 source ./sequenceFunction.sh
 runFunctionSequenced "${0##*/}" createNoisePdf "$@"
-printf "./pdf directory owned by root change it by command:\nsudo chown -R \$USER:\$USER ./pdf\n"
+
+printf "Created files are owned by root, change that by command:\nsudo chown -R \$USER:\$USER ./pdf\n"
